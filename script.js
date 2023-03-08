e84bd51322f0796df69ab8206c458c6e09213da1
Oksana Banshchykova <88445174+oxica@users.noreply.github.com>
2023-03-08 22:56:29 +0100

    04

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ const tabMenu = document.querySelector(".tab-menu");
 const btnRight = document.querySelector(".right-btn");
 
 const IconVisibility = () => {
-    let scrollLeftValue = tabMenu.scrollLeft;
+    let scrollLeftValue = Math.ceil(tabMenu.scrollLeft);
+    let scrollableWidth = tabMenu.scrollWidth - tabMenu.clientWidth;
 
     btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
+    btnRight.style.display = scrollableWidth > scrollLeftValue ? "block" : "none";
 }
 
 btnRight.addEventListener("click", () => {
     tabMenu.scrollLeft += 150;
+    setTimeout(() => IconVisibility(),50)
 });
 
 btnLeft.addEventListener("click", () => {
     tabMenu.scrollLeft -= 150;
+    setTimeout(() => IconVisibility(), 50)
 });
+
+window.onload = function(){
+    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+};
