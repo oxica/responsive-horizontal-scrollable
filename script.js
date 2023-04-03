f2533b59742338bd0e4907104f3ae343bae106da
Oksana Banshchykova <88445174+oxica@users.noreply.github.com>
2023-04-03 03:49:52 +0200

    05

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const IconVisibility = () => {
 
     btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
     btnRight.style.display = scrollableWidth > scrollLeftValue ? "block" : "none";
-}
-
+};
+    
 btnRight.addEventListener("click", () => {
     tabMenu.scrollLeft += 150;
     setTimeout(() => IconVisibility(),50)
@@ -22,4 +22,34 @@ btnLeft.addEventListener("click", () => {
 
 window.onload = function(){
     btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+    btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
 };
+
+window.onresize = function(){
+    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+    btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
+
+    let scrollLeftValue = Math.round(tabMenu.scrollLeft);
+
+    btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
+};
+
+
+let activeDrag = false;
+tabMenu.addEventListener("mousemove", (drag) => {
+    if (!activeDrag) return;
+    tabMenu.scrollLeft -= drag.movementX;
+    IconVisibility();
+    tabMenu.classList.add("dragging");
+});
+
+document.addEventListener("mouseup", () => {
+    activeDrag = false;
+    tabMenu.classList.remove("dragging");
+})
+
+tabMenu.addEventListener("mousedown", () => {
+    activeDrag = true;
+})
+
+
